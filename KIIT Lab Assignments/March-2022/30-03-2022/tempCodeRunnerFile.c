1c3bbd8eb48f320bdca544f78621aa659dfda368
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2022-04-03 17:39:09 +0530

    Enter

--- KIIT Lab Assignments/March-2022/30-03-2022/tempCodeRunnerFile.c
+++ KIIT Lab Assignments/March-2022/30-03-2022/tempCodeRunnerFile.c
@@ -1,2 +1,17 @@
-printf("Enter the number:");
-    scanf("%d",&n);
+#include<stdio.h>
+void display(int n,char ch);
+int main()
+{
+    int n;
+    char ch;
+    printf("Enter the number and the character:");
+    scanf("%d %c",&n,&ch);
+    display(n,ch);
+}
+void display(int n,char ch)
+{
+    for (int i=1;i<=n;i++)
+    {
+        printf("%c ",ch);
+    }
+}
