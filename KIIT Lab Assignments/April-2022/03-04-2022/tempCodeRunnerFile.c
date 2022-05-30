c1a72f4a7d85d73c6055764d3cb5111ae2a66cf2
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2022-05-30 22:38:43 +0530

    Enter

--- KIIT Lab Assignments/April-2022/03-04-2022/tempCodeRunnerFile.c
+++ KIIT Lab Assignments/April-2022/03-04-2022/tempCodeRunnerFile.c
@@ -1,4 +1,23 @@
-for(int j=1;j<=2*i-2;j++)
-        // {
-        //     printf(" ");
-        // }
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the  number of rows:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i-1;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=n-i+1;j++)
+        {
+            printf("%d",j);
+        }
+        for(int j=n-i;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
